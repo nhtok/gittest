d55a0f0ca54780f930f80938d6fbebef50d0ada6
Tok Nam Huat <tok_nam_huat@rp.edu.sg>
2017-03-14 10:17:37 +0800

    Documenting the comment in MainActivty.java

--- app/src/main/java/sg/edu/rp/c346/gittest/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c346/gittest/MainActivity.java
@@ -1,13 +1,11 @@
 package sg.edu.rp.c346.gittest;
-
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //Just a small change to test GitHub with this comment
     }
 }
